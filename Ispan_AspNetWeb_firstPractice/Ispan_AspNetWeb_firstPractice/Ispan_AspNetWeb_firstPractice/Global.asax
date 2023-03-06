b9f6dd943ac4eb6bb315f4945827599a4480c150
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-03-06 13:45:26 +0800

    init enpty mvc project

---
+++ Ispan_AspNetWeb_firstPractice/Ispan_AspNetWeb_firstPractice/Ispan_AspNetWeb_firstPractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ispan_AspNetWeb_firstPractice.MvcApplication" Language="C#" %>
---
+++ Ispan_AspNetWeb_firstPractice/Ispan_AspNetWeb_firstPractice/Ispan_AspNetWeb_firstPractice/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tToDo]
+(
+	[fId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [fTitle] NCHAR(10) NULL, 
+    [fLevel] NCHAR(10) NULL, 
+    [fDate] DATE NOT NULL
+)
