00c969af1ae3831de76e58148739f99139072ff8
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-03-21 10:55:15 +0800

    init

---
+++ Ispan_AspCoreWeb_SecProctice/Ispan_AspCoreWeb_SecProctice/Ispan_AspCoreWeb_SecProctice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ispan_AspCoreWeb_SecProctice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
